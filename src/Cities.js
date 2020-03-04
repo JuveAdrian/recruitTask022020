6a961a10fe79a5099b6661f89bd30b978d0e73ed
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-03-05 00:49:51 +0100

    update

--- src/Cities.js
+++ src/Cities.js
@@ -1,6 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const CitiesArr = [];
+
+
+export class Cities extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      cities: []
+    }
+  }
+
+  
+  componentDidMount() {
+    fetch('https://api.openaq.org/v1/cities')
+      .then(response => response.json())
+      .then(cities => {
+        
+        this.setState({cities: cities.results})
+      })
+      .then( console.log(this.state.cities))
+      .catch(err => console.log('Error:', err));
+  }
+
+  render() {
+    const items = this.state.cities.map((item, key) =>
+        <li>{item.name}</li>
+    );
+
+    return {
+      
+    }
+    
+};
+};
+
+/*const CitiesArr = [];
+ <ul>
+                {names.map(function(name, index){
+                    return <li key={ index }>{name}</li>;
+                  })}
+            </ul>
+
+
+
+
+
 
 const getCities = async () => {
   const api_call = await fetch('https://api.openaq.org/v1/cities');
@@ -15,12 +60,14 @@ const getCities = async () => {
 getCities();
 
 console.log(CitiesArr);
+   https://blog.hellojs.org/fetching-api-data-with-react-js-460fe8bbf8f2 
 
-export const Cities = () => (
+
+export const Cities = () => {
+
+
 
   
-      <div>
-      for(let i=0; i<CitiesArr.length)
-        <li>{CitiesArr[i]}</li>
-      </div>
-    );
+    
+};
+*/
